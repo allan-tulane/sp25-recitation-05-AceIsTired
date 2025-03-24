1d780e7a96f9c2ce0aa5aaa7e4e13c83e6bea86e
AceIsTired <162749835+AceIsTired@users.noreply.github.com>
2025-03-24 15:54:01 -0500

    Add files via upload

--- answers.md
+++ answers.md
@@ -1,8 +1,6 @@
 # CMPS 2200 Reciation 5
 ## Answers
-
-**Name:**_________________________
-
+**Name:** Jamari Ross
 
 Place all written answers from `recitation-05.md` here for easier grading.
 
@@ -13,8 +11,40 @@ Place all written answers from `recitation-05.md` here for easier grading.
 
 
 - **1b.**
+[Running] python -u "c:\Users\Jamari\Documents\Coding Projects\sp25-recitation-05-AceIsTired-main\main.py"
+
+|      n |   qsort-fixed-pivot |   qsort-random-pivot |
+|--------|---------------------|----------------------|
+|    100 |               0.010 |                0.065 |
+|    200 |               0.015 |                0.151 |
+|    500 |               0.037 |                0.401 |
+|   1000 |               0.067 |                0.839 |
+|   2000 |               0.154 |                2.125 |
+|   5000 |               0.304 |                6.666 |
+|  10000 |               0.634 |               13.182 |
+|  20000 |               1.501 |               28.177 |
+|  50000 |               4.634 |               54.768 |
+| 100000 |               8.296 |              168.499 |
+
+Both fixed and random qsort run at $O(n \log n)$ average time complexity, but qsort-fixed-pivot generally runs faster as the list size increased due to it not picking poor pivot selection.
 
 
 
 
 - **1c.**
+[Running] python -u "c:\Users\Jamari\Documents\Coding Projects\sp25-recitation-05-AceIsTired-main\main.py"
+
+|      n |   qsort-fixed-pivot |   qsort-random-pivot |   tim-sort |     ssort |
+|--------|---------------------|----------------------|------------|-----------|
+|    100 |               0.091 |                0.109 |      0.006 |     0.135 |
+|    200 |               0.174 |                0.215 |      0.012 |     0.540 |
+|    500 |               0.533 |                0.672 |      0.033 |     2.995 |
+|   1000 |               1.503 |                1.458 |      0.077 |    11.375 |
+|   2000 |               2.802 |                3.302 |      0.183 |    37.971 |
+|   5000 |               6.858 |                8.385 |      0.433 |   274.947 |
+|  10000 |              15.124 |               18.230 |      0.963 |  1065.478 |
+|  20000 |              34.326 |               35.467 |      2.048 |  4910.873 |
+|  50000 |              90.198 |              108.300 |      7.859 | 34204.204 |
+| 100000 |             208.464 |              213.987 |     12.978 |           |
+
+Python's timsort function is still faster than both quicksort and selection sort implementations.
--- main.py
+++ main.py
@@ -1,39 +1,35 @@
 import random, time
 import tabulate
+import sys
+
+
+sys.setrecursionlimit(100000)
 
 def ssort(L):
     ### selection sort
-    if (len(L) == 1):
+    if (len(L) <= 1):
         return(L)
     else:
         m = L.index(min(L))
-        print('selecting minimum %s' % L[m])       
+        # print('selecting minimum %s' % L[m])
         L[0], L[m] = L[m], L[0]
-        print('recursively sorting L=%s\n' % L[1:])
-        return [L[0]] + selection_sort(L[1:])
-        
+        # print('recursively sorting L=%s\n' % L[1:])
+        return [L[0]] + ssort(L[1:])
+
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
-    
+    if len(a) <= 1: #base case
+        return a
+
+    pivot = pivot_fn(a) #pick the pivot using the provided func
+
+    left = [x for x in a if x < pivot]
+    middle = [x for x in a if x == pivot]
+    right = [x for x in a if x > pivot]
+
+    return qsort(left, pivot_fn) + middle + qsort(right, pivot_fn)
+
+
 def time_search(sort_fn, mylist):
-    """
-    Return the number of milliseconds to run this
-    sort function on this list.
-
-    Note 1: `sort_fn` parameter is a function.
-    Note 2: time.time() returns the current time in seconds. 
-    You'll have to multiple by 1000 to get milliseconds.
-
-    Params:
-      sort_fn.....the search function
-      mylist......the list to search
-      key.........the search key 
-
-    Returns:
-      the number of milliseconds it takes to run this
-      search function on this input.
-    """
     start = time.time()
     sort_fn(mylist)
     return (time.time() - start) * 1000
@@ -50,27 +46,37 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
       for each method to run on each value of n
     """
     ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
+    
+
     result = []
     for size in sizes:
         # create list in ascending order
         mylist = list(range(size))
         # shuffles list if needed
-        #random.shuffle(mylist)
+        random.shuffle(mylist)
+
+        qsort_fixed_pivot = time_search(lambda a: qsort(a, lambda b: b[0]), mylist)
+        qsort_random_pivot = time_search(lambda a: qsort(a, lambda b: random.choice(b)), mylist)
+        tim_sort = time_search(lambda a: sorted(a), mylist)
+        s_sort = time_search(ssort, mylist) if int(size) <= 50000 else None
+
         result.append([
             len(mylist),
-            time_search(qsort_fixed_pivot, mylist),
-            time_search(qsort_random_pivot, mylist),
+            qsort_fixed_pivot,
+            qsort_random_pivot,
+            tim_sort,
+            s_sort
+            
         ])
+
+        
     return result
     ###
 
 def print_results(results):
     """ change as needed for comparisons """
     print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
+                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot', 'tim-sort', 'ssort'],
                             floatfmt=".3f",
                             tablefmt="github"))
 
